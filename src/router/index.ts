35189fb7e79fdb05ccc043379b59fc3279780617
Kaliszby <sumyung17qwq@gmail.com>
2021-12-11 15:28:49 +0700

    beginning

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,16 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/page1',
+    name: 'Page1',
+    component: () => import('../views/Pages/Page1.vue')
+  },
+  {
+    path: '/details/:id',
+    name: 'Details',
+    component: () => import('../views/Pages/Details.vue')
   }
 ]
 
